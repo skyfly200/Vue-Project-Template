9c8fb423384e899f266602781283a4ec3742f1d4
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-05 04:27:24 -0700

    setup dependencies and plugins, Convert Home.vue to use pug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  div(class="home")
+    img(alt="Vue logo" src="../assets/logo.png")
+    HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
