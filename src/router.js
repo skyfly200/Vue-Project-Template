cf4f9e220cc768a548f0882b9bd7bd47abecb704
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-07 01:35:34 -0700

    added 404 route and component

--- src/router.js
+++ src/router.js
@@ -21,6 +21,11 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "*",
+      name: "404",
+      component: () => import(/* webpackChunkName: "404" */ "./views/404.vue")
     }
   ]
 });
