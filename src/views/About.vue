d2798e70fe935ee59e28cb20898b6592d4dab3ac
Skyler Fly-Wilson <skyfly200@gmail.com>
2018-11-07 00:05:54 -0700

    tweaks to formating, add style to about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,3 +2,11 @@
   .about
     h1 This is an about page
 </template>
+
+<style scoped lang="sass">
+  .about
+    color: #FFFFFF
+    background-color: #666666
+    width: 50%
+    margin: 0 25%
+</style>
